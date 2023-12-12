e462c72939cb234a7323210dc8543e33033561d1
Natdanai <s6304062620070@email.kmutnb.ac.th>
2023-12-12 22:32:44 +0700

    hi x2

--- server.js
+++ server.js
@@ -33,9 +33,9 @@ app.post('/checkinguser', (req, res) => {
       res.status(500).send({ exist: false, error: 'Internal Server Error' });
     } else {
       if (result.length > 0) {
-        res.send({ email: result[0].member_email , exist: true });
+        res.send({ email: result[0].member_email , exist: false });
       } else {
-        res.send({ username: username , exist: false });
+        res.send({ username: username , exist: true });
       }
     }
   });
@@ -50,12 +50,49 @@ app.post('/checkingemail', (req, res) => {
       res.status(500).send({ exist: false, error: 'Internal Server Error' });
     } else {
       if (result.length > 0) {
-        res.send({ username: result[0].member_username , exist: true });
+        res.send({ username: result[0].member_username , exist: false });
       } else {
-        res.send({ email: email ,exist: false });
+        res.send({ email: email ,exist: true });
       }
     }
   });
 });
+
+app.post('/register', (req, res) => {
+  const { username, email, password, firstName, lastName, tel } = req.body;
+
+  if (!username || !email || !password || !firstName || !lastName || !tel) {
+    return res.status(400).send({ exist: false, error: 'Missing required fields' });
+  }
+  db.query('SELECT MAX(member_id) as maxId FROM members', (err, result) => {
+    if (err) {
+      console.log(err);
+      return res.status(500).send({ exist: false, error: 'Internal Server Error' });
+    }
+
+    let nextId = 'MEM001'; 
+    if (result[0].maxId) {
+      const currentId = result[0].maxId;
+      const numericPart = parseInt(currentId.substring(3), 10) + 1;
+      nextId = 'MEM' + numericPart.toString().padStart(3, '0');
+    }
+    if (result.length > 0) {
+      return res.status(409).send({ exist: false, error: 'Username or email already exists' });
+    }
+
+    db.query(
+      'INSERT INTO members (member_id, member_username, member_email, member_password, member_name, member_phone, member_follows) VALUES (?, ?, ?, ?, ?, ?, ?)',
+      [nextId, username, email, password, firstName + ' ' + lastName, tel, null],
+      (err, result) => {
+        if (err) {
+          console.log(err);
+          return res.status(500).send({ exist: false, error: 'Internal Server Error' });
+        }
+        res.status(201).send({ exist: true });
+      }
+    );
+  });
+});
+
 
 app.listen(3001, () => console.log('Avalable 3001'));
