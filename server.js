f051a0cf4c2732ea845f0e78c3619af77403958f
Mercuryheart123 <s6304062610015@email.kmutnb.ac.th>
2024-03-13 00:50:17 +0700

    asfasff

--- server.js
+++ server.js
@@ -3895,13 +3895,14 @@ app.get("/allsum", async (req, res) => {
     const results = await usePooledConnectionAsync(async (db) => {
       return new Promise((resolve, reject) => {
         db.query(
-          `SELECT os.member_id, oi.product_id, COUNT(oi.quantity) AS total_quantity, SUM(oi.quantity * oi.price) AS total_price
+          `SELECT oi.product_id, oi.quantity as total_quantity, SUM( oi.price) AS total_price,p.product_name, c.category_name
           FROM order_sumary os
           JOIN order_items oi ON os.id = oi.order_id
           JOIN products p ON oi.product_id = p.product_id
           JOIN farmers f ON p.farmer_id = f.id
+          JOIN categories c ON c.category_id = p.category_id
           WHERE os.status = 'complete' AND f.id = ?
-          GROUP BY os.member_id, oi.product_id;`,
+          GROUP BY oi.product_id order by total_quantity desc;`,
           [decoded.ID],
           (err, result) => {
             if (err) {
@@ -3915,17 +3916,17 @@ app.get("/allsum", async (req, res) => {
     });
 
     // นำผลลัพธ์ที่ได้มาประมวลผล
-    const processedResults = {};
-    results.forEach((row) => {
-      const { product_id, total_quantity, total_price } = row;
-      if (!processedResults[product_id]) {
-        processedResults[product_id] = { total_quantity: 0, total_price: 0 };
-      }
-      processedResults[product_id].total_quantity += total_quantity;
-      processedResults[product_id].total_price += parseFloat(total_price); // หรือ Number(total_price)
-    });
+    // const processedResults = {};
+    // results.forEach((row) => {
+    //   const { product_id, total_quantity, total_price } = row;
+    //   if (!processedResults[product_id]) {
+    //     processedResults[product_id] = { total_quantity: 0, total_price: 0 };
+    //   }
+    //   processedResults[product_id].total_quantity = total_quantity;
+    //   processedResults[product_id].total_price += parseFloat(total_price); // หรือ Number(total_price)
+    // });
 
-    res.status(200).json({ success: true, data: processedResults });
+    res.status(200).json({ success: true, data: results });
   } catch (error) {
     console.error(error);
     res.status(500).json({ success: false, message: "Internal Server Error" });
